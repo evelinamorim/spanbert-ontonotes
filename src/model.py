0a93cc9dd835a37ed3ce36de8f0359da789662b9
Evelin Amorim <evelin.f.amorim@inescted.pt>
2024-09-16 17:17:36 +0100

    convert genre to tensor

--- src/model.py
+++ src/model.py
@@ -230,7 +230,7 @@ class SpanBERTCorefModel(nn.Module):
         top_span_mention_scores = torch.gather(candidate_mention_scores, 0, top_span_indices.long())  # [k]
 
         genre_embeddings = nn.Parameter(torch.randn(len(self.config.GENRES), self.config.FEATURE_SIZE) * 0.02)
-        print("-->", genre)
+        genre = torch.tensor(genre, dtype=torch.long)
         genre_emb = torch.gather(genre_embeddings, 0, genre.unsqueeze(0)).squeeze(0)  # [emb]
 
         return sequence_output
