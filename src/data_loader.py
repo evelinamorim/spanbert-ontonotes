6e2cf96be93b51ca2bfbb3d93fcb727db2071282
Evelin Amorim <evelin.f.amorim@inescted.pt>
2024-10-02 16:07:09 +0100

    add the flag training as tensor to the batch

--- src/data_loader.py
+++ src/data_loader.py
@@ -38,6 +38,7 @@ def collate_fn(batch):
     speaker_ids_padded = pad_to_max_len(speaker_ids,  config.MAX_NUM_SENTENCES)
 
     genre_tensor = torch.tensor(genre)
+    is_training_tensor = torch.tensor(is_training)
 
     gold_starts_padded = pad_to_max_len(gold_starts,  config.MAX_NUM_CLUSTERS)
     gold_ends_padded = pad_to_max_len(gold_ends,  config.MAX_NUM_CLUSTERS)
@@ -45,7 +46,7 @@ def collate_fn(batch):
 
     sentence_map_padded = pad_to_max_len(sentence_map,  config.MAX_NUM_WORDS)
 
-    return (input_ids_padded, input_mask_padded, text_len_padded, speaker_ids_padded, genre_tensor, gold_starts_padded, gold_ends_padded, cluster_ids_padded, sentence_map_padded)
+    return (input_ids_padded, input_mask_padded, text_len_padded, speaker_ids_padded, genre_tensor, is_training_tensor, gold_starts_padded, gold_ends_padded, cluster_ids_padded, sentence_map_padded)
 
 class CorefDataset(torch.utils.data.Dataset):
     def __init__(self, tokenizer, config, split):
